47888bd97bea66f40cec6a0e63b360c720d6a9d8
HADMARINE <tsx@kakao.com>
2021-09-02 14:04:14 +0900

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
+var fs = require('fs');
+
+gulp.task('build', function () {
+  if (fs.existsSync('dist')) {
+    fs.rmSync('dist', { recursive: true });
+  }
+
+  var merge = require('merge2');
+  var tsProject = ts.createProject('tsconfig.json');
+
+  var tsResult = tsProject.src().pipe(tsProject());
+
+  return merge([
+    tsResult.dts.pipe(gulp.dest(tsProject.config.compilerOptions.outDir)),
+    tsResult.js.pipe(gulp.dest(tsProject.config.compilerOptions.outDir)),
+  ]);
+});
